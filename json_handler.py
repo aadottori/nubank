b769ced1ef0321f17b411f06ce3e5f22c704e96a
Antonio Dottori <aadottori@gmail.com>
2022-05-06 17:48:20 -0300

    organized files

--- json_handler.py
+++ json_handler.py
@@ -1,6 +1,6 @@
 import json
 
 
-def create_json(function):
-    with open("results/{}.json".format(function.__name__), 'w', encoding='utf-8') as f:
+def create_json(function, user):
+    with open(f"results/{user}/{function.__name__}.json", 'w', encoding='utf-8') as f:
         json.dump(function(), f, ensure_ascii=False, indent=4)
--- main.py
+++ main.py
@@ -1,57 +1,58 @@
 from pynubank import Nubank, MockHttpClient
-import config
+from info import users
 from json_handler import create_json
 import types
 
 
-#nu = Nubank(MockHttpClient())
-nu = Nubank()
-nu.authenticate_with_cert(config.login_info["user"], config.login_info["password"], config.login_info["authentication"])
+for user in users:
+    nu = Nubank()
+    nu.authenticate_with_cert(users[user]["cpf"], 
+                            users[user]["password"], 
+                            users[user]["p12_path"])
 
 
-def card_statements():
-    return nu.get_card_statements()
+    def card_statements():
+        return nu.get_card_statements()
 
-def transactions():
-    return nu.get_account_statements()
+    def transactions():
+        return nu.get_account_statements()
 
-def bills():
-    return nu.get_bills()
+    def bills():
+        return nu.get_bills()
 
-def card_feed():
-    return nu.get_card_feed()
+    def card_feed():
+        return nu.get_card_feed()
 
-def card_payments():
-    return nu.get_card_payments()
+    def card_payments():
+        return nu.get_card_payments()
 
-def customer():
-    return nu.get_customer()
+    def customer():
+        return nu.get_customer()
 
-def bill_details(bill):
-    return nu.get_bill_details(bill)
+    def bill_details(bill):
+        return nu.get_bill_details(bill)
 
-def account_feed():
-    return nu.get_account_feed()
+    def account_feed():
+        return nu.get_account_feed()
 
-def account_statements():
-    return nu.get_account_statements()
+    def account_statements():
+        return nu.get_account_statements()
 
-def account_balance():
-    return nu.get_account_balance()
+    def account_balance():
+        return nu.get_account_balance()
 
-def account_investments_details():
-    return nu.get_account_investments_details()
+    def account_investments_details():
+        return nu.get_account_investments_details()
 
-def account_investments_yield():
-    return nu.get_account_investments_yield()
+    def account_investments_yield():
+        return nu.get_account_investments_yield()
 
-def available_pix_keys():
-    return nu.get_available_pix_keys()
+    def available_pix_keys():
+        return nu.get_available_pix_keys()
 
 
-defined_functions = [f for f in globals().values() if type(f) == types.FunctionType]
-functions_not_to_call = [create_json, bill_details]
+    defined_functions = [f for f in globals().values() if type(f) == types.FunctionType]
+    functions_not_to_call = [create_json, bill_details]
 
-for function in defined_functions:
-    if function not in functions_not_to_call:
-        create_json(function)
+    for function in [x for x in defined_functions if x not in functions_not_to_call]:
+        create_json(function, user)
